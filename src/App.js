23ba74bae6fcbaeb35fcb5ad8f5837ecf47d2c5d
shawna-donnelly <shawna.donnelly@froedtert.com>
2022-07-29 14:55:14 -0600

    add checkbox and tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [bgColor, setBgColor] = useState("red");
+  const [checked, setChecked] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{ backgroundColor: bgColor }}
+        onClick={() => {
+          setBgColor(bgColor === "blue" ? "red" : "blue");
+        }}
+        disabled={checked}
+      >
+        Change to {bgColor === "blue" ? "red" : "blue"}
+      </button>
+      <input
+        id="disable-button-checkbox"
+        type="checkbox"
+        defaultChecked={checked}
+        aria-checked={checked}
+        onChange={() => setChecked(!checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable Button</label>
     </div>
   );
-}
+};
 
 export default App;
